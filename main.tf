b6552f670dac26b72bed2a199b0d70180ad5c7ff
Prathima Joginipelly <75338982+prathimaautomation@users.noreply.github.com>
2021-08-17 15:53:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,48 @@
 # Let's build a script to connect to AWS and download/setup all dependencies required
-# keyword: provider aws
 provider "aws" {
        region = "eu-west-1"	
 }
 
 # Let's build a vpc 
-resource "aws_vpc" "terraform_vpc_code_test" {
+resource "aws_vpc" "eng89_prathima_terraform_vpc" {
   cidr_block       = var.cidr_block 
   instance_tenancy = "default"
-  enable_dns_support = true # gives you an internal domain name
-  enable_dns_hostnames = true # gives you an internal host name
+  #enable_dns_support = true # gives you an internal domain name
+  #enable_dns_hostnames = true # gives you an internal host name
 
   tags = {
     Name = var.vpc_name
   }
 }
 
+resource "aws_internet_gateway" "eng89_prathima_tf_igw" {
+    vpc_id = aws_vpc.eng89_prathima_terraform_vpc.id
+    tags = {
+        Name = var.igw_name
+    }
+}
+
+resource "aws_route_table" "eng89_prathima_terraform_rtb" {
+    vpc_id = aws_vpc.eng89_prathima_terraform_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.eng89_prathima_tf_igw.id
+    }
+    tags = {
+        Name = "eng89_prathima_terraform_rtb"
+    }
+}
+
+resource "aws_route_table_association" "eng89_prathima_tf_rtb_assoc_public" {
+    subnet_id = aws_subnet.eng89_prathima_app_subnet.id
+    route_table_id = aws_route_table.eng89_prathima_terraform_rtb.id
+}
+
 #Let's create subnet for the app
-resource "aws_subnet" "app_subnet"{
-       vpc_id = var.vpc_id
-       cidr_block = "10.210.2.0/24"
+resource "aws_subnet" "eng89_prathima_app_subnet"{
+       vpc_id = aws_vpc.eng89_prathima_terraform_vpc.id
+       cidr_block = "10.210.1.0/24"
+       map_public_ip_on_launch = true
        availability_zone = "eu-west-1a"
        tags = {
               Name = "eng89_prathima_terraform_app_subnet"
@@ -28,24 +51,30 @@ resource "aws_subnet" "app_subnet"{
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "terraform_igw"{
-       vpc_id = aws_vpc.terraform_vpc_code_test.id
+       vpc_id = aws_vpc.eng89_prathima_terraform_vpc.id
 
        tags = {
               Name = var.igw_name
        }
 }
 
-resource "aws_security_group" "pub_sec_group"{
+resource "aws_security_group" "eng89_prathima_public_security_group"{
        name        = "eng89_prathima_terraform_SG_app"
        description = "app security group"
-       vpc_id      = aws_vpc.terraform_vpc_code_test.id
+       vpc_id      = aws_vpc.eng89_prathima_terraform_vpc.id
 
 # Inbound rules for webapp
+ingress {
+       from_port      = 22
+       to_port        = 22
+       protocol       = "tcp"
+       cidr_blocks    = [var.my_ip]
+}
 # Inbound rules code block:
 ingress { #ingress is for inbound rules
   from_port  = 80 # for our app to launch in the browser
   to_port    = 80 # for our app to launch in the browser
-  protocol   = "HTTP"
+  protocol   = "tcp"
   cidr_blocks = ["0.0.0.0/0"] # allow all
 }
 ingress { 
@@ -61,12 +90,11 @@ ingress {
   cidr_blocks = ["0.0.0.0/0"] # allow all
 }
 
-
 # Outbound rules code block
 egress { # egress for outbound rules
   from_port   = 0
   to_port     = 0
-  protocol    = "-1" # allow all
+  protocol    = -1 # allow all
   cidr_blocks = ["0.0.0.0/0"]
 
 }
@@ -76,22 +104,13 @@ tags = {
 
 }
 
-resource "aws_security_group_rule" "my_ssh" {
-       type           = "ingress"
-       from_port      = 22
-       to_port        = 22
-       protocol       = "tcp"
-       cidr_blocks    = [var.my_ip]
-       security_group_id = aws_security_group.pub_sec_group.id
-}
-
 resource "aws_security_group_rule" "vpc_access" {
        type           = "ingress"
        from_port      = 0
        to_port        = 0
        protocol       = "-1"
        cidr_blocks    = [var.cidr_block]
-       security_group_id = aws_security_group.pub_sec_group.id
+       security_group_id = aws_security_group.eng89_prathima_public_security_group.id
 }
 # keyword called "resource" provide resource name and give name with specific details to the service
 resource "aws_instance" "app_instance" {
@@ -99,11 +118,9 @@ resource "aws_instance" "app_instance" {
 # resource aws_ec2_instance, name it as eng89_prathima_terraform, ami, type of instance, with or without ip,
 # provide valid ami id
 ami = "ami-026754d4887301d2a"
-
 # provide what type of instance you would like to create 
 instance_type = "t2.micro"
-subnet_id = aws_subnet.app_subnet.id
-
+subnet_id = aws_subnet.eng89_prathima_app_subnet.id
 # we would like public ip for this instance
 associate_public_ip_address = true
 
